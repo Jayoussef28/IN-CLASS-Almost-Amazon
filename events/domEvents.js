5f2debee1436b0443466e105ef566a075c8e5904
Jordan Youssef <jordanyoussef28@gmail.com>
2024-05-25 11:02:51 -0500

    fixed domEvent and StartApp errors

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,10 +1,10 @@
-import  showBooks  from '../pages/books';
+import { showBooks } from '../pages/books';
 import { getBooks, deleteBook, getSingleBook } from '../api/bookData';
 import addBookForm from '../components/forms/addBookForm';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
-    // TODO: CLICK EVENT FOR DELETING A BOOK
+    // TODO: CLICK EVENT FOR DELETING A BOOK---DONE
     if (e.target.id.includes('delete-book')) {
       // eslint-disable-next-line no-alert
       if (window.confirm('Want to delete?')) {
@@ -25,17 +25,14 @@ const domEvents = () => {
 
     // TODO: CLICK EVENT EDITING/UPDATING A BOOK
     if (e.target.id.includes('edit-book-btn')) {
-        const [, firebaseKey] = e.target.id.split('--');
+      const [, firebaseKey] = e.target.id.split('--');
 
-        getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
-      }
-
+      getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
       console.warn('EDIT BOOK', e.target.id);
-      console.warn(e.target.id.split('--'));
     }
 
     // TODO: CLICK EVENT FOR VIEW BOOK DETAILS
-   if (e.target.id.includes('view-book-btn')) {
+    if (e.target.id.includes('view-book-btn')) {
       console.warn('VIEW BOOK', e.target.id);
       console.warn(e.target.id.split('--'));
     }
--- utils/startApp.js
+++ utils/startApp.js
@@ -2,8 +2,8 @@ import { getBooks } from '../api/bookData';
 import logoutButton from '../components/buttons/logoutButton';
 import domBuilder from '../components/shared/domBuilder';
 import navBar from '../components/shared/navBar';
-import domEvents from '../events/domEvents';
 import formEvents from '../events/formEvents';
+import domEvents from '../events/domEvents';
 import navigationEvents from '../events/navigationEvents';
 import { showBooks } from '../pages/books';
 
